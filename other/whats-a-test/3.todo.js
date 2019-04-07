6b415c263d05bb13690cda6651ae6a4c57d6395b
HaQadosch Berraka <haqadosch@pm.me>
2019-04-07 19:22:36 +0100

    Module 1 done.

--- other/whats-a-test/3.todo.js
+++ other/whats-a-test/3.todo.js
@@ -25,24 +25,35 @@ Then run this code with `node 3.todo`
 
  */
 
-const {sum, subtract} = require('./math')
+const { sum, subtract } = require('./math')
 
-let result, expected
-
-result = sum(3, 7)
-expected = 10
-expect(result).toBe(expected)
-
-result = subtract(7, 3)
-expected = 4
-expect(result).toBe(expected)
-
-function expect(actual) {
-  return {
-    toBe(expected) {
-      if (actual !== expected) {
-        throw new Error(`${actual} is not equal to ${expected}`)
-      }
-    },
+const expect = actual => ({
+  toBe: expected => {
+    if (actual !== expected) {
+      throw new Error(`${actual} is not equal to ${expected}`)
+    }
   }
+})
+
+const test = (title, foo) => {
+  let errMessage = ` ✔ All Good.`
+  try {
+    foo()
+  } catch (error) {
+    errMessage = error
+  }
+
+  console.log(`${title}: ${errMessage}`)
 }
+
+test('sum', _ => {
+  const result = sum(3, 7)
+  const expected = 10
+  expect(result).toBe(expected)
+})
+
+test('substract', _ => {
+  const result = subtract(7, 3)
+  const expected = 4
+  expect(result).toBe(expected)
+})
